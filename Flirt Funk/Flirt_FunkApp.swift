b461b028ff4330820590778ea912d9a0775773c2
Alexandre Samson <alexander_samson@icloud.com>
2024-01-22 17:16:55 +0100

    Initial Commit

---
+++ Flirt Funk/Flirt_FunkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flirt_FunkApp.swift
+//  Flirt Funk
+//
+//  Created by Alexandre Samson on 22.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Flirt_FunkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
