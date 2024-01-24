bc99c338cedc194578b6ce59d6950f53d4ea3011
Alexandre Samson <alexander_samson@icloud.com>
2024-01-24 19:46:58 +0100

    Ein stück Weiter

---
+++ Flirt Funk/Service/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  Flirt Funk
+//
+//  Created by Alexandre Samson on 24.01.24.
+//
+
+import Foundation
---
+++ Flirt Funk/Views/Auth/RegisterImageView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterImageView.swift
+//  Flirt Funk
+//
+//  Created by Alexandre Samson on 24.01.24.
+//
+
+import SwiftUI
+
+struct RegisterImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterImageView()
+}
