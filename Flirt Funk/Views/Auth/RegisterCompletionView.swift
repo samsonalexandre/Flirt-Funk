ba11cc4a165ebb6dbb83b94143b3b31a28857e40
Alexandre Samson <alexander_samson@icloud.com>
2024-01-29 15:26:25 +0100

    Register Flow fertig

---
+++ Flirt Funk/Views/Auth/RegisterCompletionView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterCompletionView.swift
+//  Flirt Funk
+//
+//  Created by Alexandre Samson on 29.01.24.
+//
+
+import SwiftUI
+
+struct RegisterCompletionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterCompletionView()
+}
